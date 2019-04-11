568102d549addddf6cb85cfd8693381dd395bbc4
muhammad <muhammad@g3t.co.uk>
2019-04-11 17:37:44 +0100

    change

--- jscripts/clander.js
+++ jscripts/clander.js
@@ -1,11 +1,10 @@
-$(document).ready(function(){
-$('#date').datepicker({
-  showOn: 'both',
-  buttonImage: 'calendar.png',
-  buttonImageOnly: true,
-  changeMonth: true,
-  changeYear: true,
-  dateFormat: 'yy-mm-dd'
+$(document).ready(function() {
+    $('#date').datepicker({
+        showOn: 'both',
+        buttonImage: 'images/calendar.png',
+        buttonImageOnly: true,
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: 'yy-mm-dd'
+    });
 });
-});
-
